e4ef4a511d9d69bae49816e38077ccc045adc7e0
Mike Yates <mike@yatesdev.com>
2019-12-10 12:30:27 -0600

    Create ConnectionManager

--- src/api/src/index.ts
+++ src/api/src/index.ts
@@ -1,6 +1,9 @@
 import { ITemplate } from '@yatesdev/sineboard-core';
+import { ConnectionManager } from './connection';
 import { TemplateInitializer } from './template';
 import { flatten } from './util';
 
-const templateListener = new TemplateInitializer();
+const connectionManager = new ConnectionManager();
+
+const templateListener = new TemplateInitializer(connectionManager);
 templateListener.start();
--- src/api/src/template/index.ts
+++ src/api/src/template/index.ts
@@ -1,44 +1,37 @@
 import Redis from 'ioredis';
 
 import { Events, ITemplate } from '@yatesdev/sineboard-core';
+import { ConnectionManager } from 'connection';
 import { flatten, moduleLoader } from '../util';
 
 import { Canvas } from 'canvas';
 import * as fs from 'fs';
 
 export class TemplateInitializer {
-  templateListener: Redis.Redis;
-  redis: Redis.Redis;
-
-  constructor(configOverrides?: any) {
-    const config = Object.assign({
-      host: '127.0.0.1',
-      posrt: 6379,
-    }, configOverrides);
-
-    this.redis = new Redis(config);
-    this.templateListener = this.redis.duplicate();
-  }
+  constructor(
+    private readonly connectionManager: ConnectionManager,
+  ) {}
 
   start() {
-    this.templateListener.subscribe(Events.ConfigurationLoaded);
-    this.templateListener.on('message', (channel, configurationKey) => {
+    this.connectionManager.subscribeToChannel(Events.ConfigurationLoaded);
+    this.connectionManager.channels.get(Events.ConfigurationLoaded).on('message', (channel, configurationKey) => {
       this.loadClientConfiguration(configurationKey);
     });
 
     const canvas = new Canvas(720, 720);
     const context = canvas.getContext('2d');
     context.fillRect(0, 0, 50, 50);
+    context.fillRect(100, 100, 150, 150);
     const exportBuffer = canvas.toBuffer();
 
-    this.redis.pipeline()
+    this.connectionManager.redis.pipeline()
       .setBuffer('exportTest', exportBuffer)
       .publish(Events.TemplateRendered, 'exportTest')
       .exec();
   }
 
   private async loadClientConfiguration(redisKey: string) {
-    const configuration = JSON.parse(await this.redis.get(redisKey)) as ITemplate;
+    const configuration = JSON.parse(await this.connectionManager.redis.get(redisKey)) as ITemplate;
     // grab datasources
     flatten(configuration).map((x) => x.dataSource);
   }
