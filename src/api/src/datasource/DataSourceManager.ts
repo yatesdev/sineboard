534aa3cd286ef9dddb7b41045c118ad39b726a7b
Mike Yates <mike@yatesdev.com>
2019-12-15 23:43:23 -0500

    Update datasource job creation to allow callback after fetch
    Also create unique hash to grab jobs by

--- src/api/src/datasource/DataSourceManager.ts
+++ src/api/src/datasource/DataSourceManager.ts
@@ -1,14 +1,17 @@
 import { Events, IDataSource } from '@yatesdev/sineboard-core';
 import Schedule from 'node-schedule'; // Need to figure out how to make this typeable or instanceable
+import hash from 'object-hash';
 import { ConnectionManager } from '../connection';
 
 export class DataSourceManager {
+  jobs: Map<string, Schedule.Job>;
   dataSources: IDataSource[];
   scheduler: any; // technically the Schedule type
 
   constructor(
     private readonly connectionManager: ConnectionManager,
   ) {
+    this.jobs = new Map();
     this.dataSources = [];
     this.scheduler = Schedule;
   }
@@ -19,12 +22,21 @@ export class DataSourceManager {
     // });
   }
 
-  add(dataSource: IDataSource, startDate: string, endDate: string, updateFrequency: string | number) {
-    const job = this.scheduler.scheduleJob({
-      start: startDate,
-      end: endDate,
-      rule: updateFrequency,
-    }, dataSource.fetch);
-    console.log(job);
+  add(dataSource: IDataSource, startDate: string, endDate: string, updateFrequency: string | number, onFetch: () => void) {
+    const hashId = hash(dataSource.name, { 
+      excludeKeys: (key: string) => key !== 'name' && key !== 'options' && key !== 'updateFrequency' },
+    );
+    const job = this.scheduler.scheduleJob(
+      hashId,
+      {
+        start: startDate,
+        end: endDate,
+        rule: updateFrequency,
+      }, async () => {
+        await dataSource.fetch();
+        onFetch();
+      });
+    this.jobs.set(hashId, job);
+    console.log(this.jobs.entries());
   }
 }
