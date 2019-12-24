0cbcf62cd64385d098eec0aed5d75f428fda6296
Mike Yates <mike@yatesdev.com>
2019-12-23 20:43:07 -0500

    Extract Template Compositor

--- packages/sineboard-api/src/template/index.ts
+++ packages/sineboard-api/src/template/index.ts
@@ -1,11 +1,14 @@
 import { Events, IPageDisplay, ITemplate } from '@yatesdev/sineboard-core';
 import { Logger } from '@yatesdev/sineboard-log';
 import { Canvas } from 'canvas';
-// import * as fs from 'fs';
+
+import * as fs from 'fs';
+
 import { ConnectionManager } from '../connection';
 import { DataSourceManager } from '../datasource';
 import { flatten } from '../util';
 
+import { TemplateCompositor } from './TemplateCompositor';
 import { TemplateFactory } from './TemplateFactory';
 
 export class TemplateInitializer {
@@ -20,20 +23,9 @@ export class TemplateInitializer {
       this.loadClientConfiguration(configurationKey);
     });
 
-    // handle api reboot and pull existing
+    // handle api reboot and pull existing client configuration
     const clientList = await this.connectionManager.redis.smembers('clients');
     clientList.forEach((clientKey) => this.loadClientConfiguration(`template:${clientKey}`));
-
-    const canvas = new Canvas(720, 720);
-    const context = canvas.getContext('2d');
-    context.fillRect(0, 0, 50, 50);
-    context.fillRect(100, 100, 150, 150);
-    const exportBuffer = canvas.toBuffer();
-
-    this.connectionManager.redis.pipeline()
-      .setBuffer('exportTest', exportBuffer)
-      .publish(Events.TemplateRendered, 'exportTest')
-      .exec();
   }
 
   private async loadClientConfiguration(redisKey: string) {
@@ -42,20 +34,8 @@ export class TemplateInitializer {
     // initialize template instances
     const temp = TemplateFactory(configuration.template);
 
+
     const dataSourceUpdated = (template: ITemplate) => {
-      const templateCompositor = (t: ITemplate): Canvas => {
-        if (Array.isArray(t.children)) {
-          const context = t.canvas.getContext('2d');
-
-          t.children.forEach((child) => {
-            templateCompositor(child);
-
-            context.drawImage(child.canvas, child.posX, child.posY);
-          });
-        }
-        return t.canvas;
-      };
-
       return async () => {
         // console.log(template.name, template.dataSource.data);
         if (template.children) {
@@ -76,12 +56,13 @@ export class TemplateInitializer {
         } while (rootNode.parent);
 
         const compositeStart = process.hrtime();
-        const output = templateCompositor(rootNode);
+        const output = TemplateCompositor(rootNode);
         const compositeEnd = process.hrtime(compositeStart);
         Logger.info(`Execution time (composition): ${compositeEnd[0]}s ${compositeEnd[1] / 1000000}ms`);
 
         const exportBuffer = output.getContext('2d').getImageData(0, 0, output.width, output.height).data; // output.toBuffer('raw');
-        // strip the A from RGBA[]
+
+        // strip the A from RGBA[] and convert Uint8ClampedArray to Uint8Array
         const rgbExportBuffer = Buffer.from(new Uint8Array(exportBuffer.filter((_: any, index: number) => (index + 1) % 4)));
 
         await this.connectionManager.redis.pipeline()
