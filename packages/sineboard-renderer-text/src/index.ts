644e055657082d272fe3da170cf9c29593d64067
Mike Yates <mike@yatesdev.com>
2019-12-29 14:02:53 -0600

    Convert font loading to synchronous
    Was running into issue where immediately triggered renders of text for static datasources did not have the fonts loaded in time.

--- packages/sineboard-renderer-text/src/index.ts
+++ packages/sineboard-renderer-text/src/index.ts
@@ -5,12 +5,10 @@ export * from './TextRenderer';
 // tslint:disable-next-line:no-var-requires
 const { registerFont } = module.parent.require('canvas');
 const fontPath = join(__dirname, './assets');
-fs.readdir(fontPath, (err, files) => {
-  console.log(files);
-  files.forEach((file) => {
-    console.log(file);
-    const fontName = RegExp('^([^.ttf]+)').exec(file)[0];
-    console.log(fontName);
-    registerFont( join(fontPath, file), { family: fontName});
-  });
+
+fs.readdirSync(fontPath).forEach((file) => {
+  console.log(file);
+  const fontName = RegExp('^([^.ttf]+)').exec(file)[0];
+  console.log(fontName);
+  registerFont( join(fontPath, file), { family: fontName});
 });
