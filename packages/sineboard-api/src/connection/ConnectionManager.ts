e9452ad33c7dcebdd3d11903de36d3e23bd72789
Mike Yates <mike@yatesdev.com>
2019-12-29 12:41:32 -0600

    Add dotenv for API configuration

--- packages/sineboard-api/src/connection/ConnectionManager.ts
+++ packages/sineboard-api/src/connection/ConnectionManager.ts
@@ -6,8 +6,8 @@ export class ConnectionManager {
 
   constructor(configOverrides?: any) {
     const config = Object.assign({
-      host: '127.0.0.1',
-      posrt: 6379,
+      host: process.env.REDIS_HOST || '127.0.0.1',
+      port: parseInt(process.env.REDIS_PORT, 10) || 6379,
     }, configOverrides);
 
     this.redis = new Redis(config);
--- packages/sineboard-api/src/index.ts
+++ packages/sineboard-api/src/index.ts
@@ -1,6 +1,12 @@
+import { config } from 'dotenv';
+import { resolve } from 'path';
+
 import { ConnectionManager } from './connection';
 import { DataSourceManager } from './datasource';
 import { TemplateInitializer } from './template';
+
+const configPath = resolve(__dirname, './.env');
+config({ path: configPath });
 
 const connectionManager = new ConnectionManager();
 
