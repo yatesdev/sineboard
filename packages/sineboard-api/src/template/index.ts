b8eb2d93c10c19d515edc7e4e82b454460b8d97f
Mike Yates <mike@yatesdev.com>
2020-01-01 09:43:38 -0600

    Allow multiple pages in config file

--- packages/sineboard-api/src/template/index.ts
+++ packages/sineboard-api/src/template/index.ts
@@ -1,6 +1,5 @@
 import { Events, IPageDisplay, ITemplate } from '@yatesdev/sineboard-core';
 import { Logger } from '@yatesdev/sineboard-log';
-import { Canvas } from 'canvas';
 
 import * as fs from 'fs';
 import * as path from 'path';
@@ -30,67 +29,72 @@ export class TemplateInitializer {
   }
 
   private async loadClientConfiguration(redisKey: string) {
-    const configuration = JSON.parse(await this.connectionManager.redis.get(redisKey)) as IPageDisplay;
+    let configuration: IPageDisplay | IPageDisplay[] = JSON.parse(await this.connectionManager.redis.get(redisKey));
 
-    // initialize template instances
-    const temp = TemplateFactory(configuration.template);
+    if (!Array.isArray(configuration)) {
+      configuration = [configuration];
+    }
 
-    const dataSourceUpdated = (template: ITemplate) => {
-      return async () => {
-        // console.log(template.name, template.dataSource.data);
-        if (template.children) {
-          template.children.forEach((child) => {
-            dataSourceUpdated(child);
-          });
+    configuration.forEach((page) => {
+      const temp = TemplateFactory(page.template);
+
+      flatten(temp).map((template) => {
+        // Immediately trigger render for static datasources
+        if (!template.dataSource.updateFrequency) {
+          this.dataSourceUpdated(template)();
+          return;
         }
-        if (template.renderer) {
-          template.renderer.render(template.canvas, template.dataSource.data);
-        }
 
-        // find rootNode to send to compositor
-        let rootNode = template;
-        do {
-          if (rootNode.parent) {
-            rootNode = rootNode.parent;
-          }
-        } while (rootNode.parent);
-
-        const compositeStart = process.hrtime();
-        const output = TemplateCompositor(rootNode);
-        const compositeEnd = process.hrtime(compositeStart);
-        Logger.info(`Execution time (composition): ${compositeEnd[0]}s ${compositeEnd[1] / 1000000}ms`);
-
-        const exportBuffer = output.getContext('2d').getImageData(0, 0, output.width, output.height).data; // output.toBuffer('raw');
-
-        // strip the A from RGBA[] and convert Uint8ClampedArray to Uint8Array
-        const rgbExportBuffer = Buffer.from(new Uint8Array(exportBuffer.filter((_: any, index: number) => (index + 1) % 4)));
-
-        await this.connectionManager.redis.pipeline()
-          .setBuffer('exportTest', rgbExportBuffer)
-          .publish(Events.TemplateRendered, 'exportTest')
-          .exec();
-
-        // DEBUG ONLY
-        const exportStream = output.createPNGStream();
-        const debugPath = path.resolve(process.cwd(), './debug', 'foo.png');
-        const fileStream = fs.createWriteStream(debugPath);
-        exportStream.pipe(fileStream);
-      };
-    };
-
-    flatten(temp).map((template) => {
-      // Immediately trigger render for static datasources
-      if (!template.dataSource.updateFrequency) {
-        dataSourceUpdated(template)();
-        return;
-      }
-
-      this.dataSourceManager.add(
-        template.dataSource,
-        configuration.schedule.startDate,
-        configuration.schedule.endDate,
-        template.dataSource.updateFrequency,
-        dataSourceUpdated(template));
+        this.dataSourceManager.add(
+          template.dataSource,
+          page.schedule.startDate,
+          page.schedule.endDate,
+          template.dataSource.updateFrequency,
+          this.dataSourceUpdated(template));
+      });
     });
   }
+
+  private dataSourceUpdated(template: ITemplate) {
+    return async () => {
+      // console.log(template.name, template.dataSource.data);
+      if (template.children) {
+        template.children.forEach((child) => {
+          this.dataSourceUpdated(child);
+        });
+      }
+      if (template.renderer) {
+        template.renderer.render(template.canvas, template.dataSource.data);
+      }
+
+      // find rootNode to send to compositor
+      let rootNode = template;
+      do {
+        if (rootNode.parent) {
+          rootNode = rootNode.parent;
+        }
+      } while (rootNode.parent);
+
+      const compositeStart = process.hrtime();
+      const output = TemplateCompositor(rootNode);
+      const compositeEnd = process.hrtime(compositeStart);
+      Logger.info(`Execution time (composition): ${compositeEnd[0]}s ${compositeEnd[1] / 1000000}ms`);
+
+      const exportBuffer = output.getContext('2d').getImageData(0, 0, output.width, output.height).data; // output.toBuffer('raw');
+
+      // strip the A from RGBA[] and convert Uint8ClampedArray to Uint8Array
+      const rgbExportBuffer = Buffer.from(new Uint8Array(exportBuffer.filter((_: any, index: number) => (index + 1) % 4)));
+
+      await this.connectionManager.redis.pipeline()
+        .setBuffer('exportTest', rgbExportBuffer)
+        .publish(Events.TemplateRendered, 'exportTest')
+        .exec();
+
+      // DEBUG ONLY
+      const exportStream = output.createPNGStream();
+      const debugPath = path.resolve(process.cwd(), './debug', 'foo.png');
+      const fileStream = fs.createWriteStream(debugPath);
+      exportStream.pipe(fileStream);
+    };
+  };
 }
