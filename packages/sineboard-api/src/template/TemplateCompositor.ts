47585d83a69912f7d5d6f4b1fde20be253c24f23
Mike Yates <mike@yatesdev.com>
2020-01-06 17:56:02 -0500

    Remove extra debugging for now

--- packages/sineboard-api/src/template/TemplateCompositor.ts
+++ packages/sineboard-api/src/template/TemplateCompositor.ts
@@ -13,10 +13,10 @@ export const TemplateCompositor = (t: ITemplate): Canvas => {
       context.drawImage(child.canvas, child.posX, child.posY);
     });
   }
-  // DEBUG ONLY
-  const exportStream = t.canvas.createPNGStream();
-  const debugPath = path.resolve(process.cwd(), './debug', `${t.name}.png`);
-  const fileStream = fs.createWriteStream(debugPath);
-  exportStream.pipe(fileStream);
+  // // DEBUG ONLY
+  // const exportStream = t.canvas.createPNGStream();
+  // const debugPath = path.resolve(process.cwd(), './debug', `${t.name}.png`);
+  // const fileStream = fs.createWriteStream(debugPath);
+  // exportStream.pipe(fileStream);
   return t.canvas;
 };
