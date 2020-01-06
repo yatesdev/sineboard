b9916408c4da958590392bcab6a36709e39d046d
Mike Yates <mike@yatesdev.com>
2020-01-06 09:44:09 -0500

    ImageData size pulled from root template size

--- packages/sineboard-client-debug/src/DebugClient.ts
+++ packages/sineboard-client-debug/src/DebugClient.ts
@@ -3,6 +3,7 @@ import fs from 'fs';
 import path from 'path';
 
 import { SineboardClientBase } from '@yatesdev/sineboard-client-base';
+import { IDisplayTemplate } from '@yatesdev/sineboard-core';
 
 export class SineboardDebugClient extends SineboardClientBase {
   private readonly canvas: Canvas;
@@ -10,30 +11,30 @@ export class SineboardDebugClient extends SineboardClientBase {
 
   constructor() {
     super();
-    this.canvas = createCanvas(100, 100);
+    this.canvas = createCanvas(this.displayWidth, this.displayHeight);
     this.context = this.canvas.getContext('2d');
   }
 
-  onDisplay(display: Buffer): void {
-    const imageData = createImageData(new Uint8ClampedArray(display), this.displayWidth, this.displayHeight);
+  onDisplay(template: IDisplayTemplate, buffer: Buffer): void {
+    const imageData = createImageData(new Uint8ClampedArray(buffer), template.template.width, template.template.height);
 
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.context.clearRect(0, 0, this.displayWidth, this.displayHeight);
     this.context.putImageData(imageData, 0, 0);
 
     this.outputToFile();
   }
 
   onEmptyDisplay(): void {
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.context.clearRect(0, 0, this.displayWidth, this.displayHeight);
     this.context.fillStyle = 'rgb(200,200,200)';
-    this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this.context.fillRect(0, 0, this.displayWidth, this.displayHeight);
 
     this.outputToFile();
   }
 
   private outputToFile() {
     const exportStream = this.canvas.createPNGStream();
-    const debugPath = path.resolve(process.cwd(), './debug', 'foo.png');
+    const debugPath = path.resolve(process.cwd(), './debug', 'output.png');
     const fileStream = fs.createWriteStream(debugPath);
     exportStream.pipe(fileStream);
   }
